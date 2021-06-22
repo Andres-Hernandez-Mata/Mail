159b82f04c6cf7d83d3e8fe35b5d6a989396e572
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-22 06:23:16 -0500

    sendmail

--- src/01_TestMail.py
+++ src/01_TestMail.py
@@ -19,4 +19,4 @@ context = ssl.create_default_context()
 
 with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
     server.login("test.pc.fcfm@gmail.com", password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail("sender_email", "receiver_email", message)
