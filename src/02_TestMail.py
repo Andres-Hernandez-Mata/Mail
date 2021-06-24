339e5a62f44bca7de60d2459995b506aea507815
Andrés Hernández Mata <andreshernandezmta@gmail.com>
2021-06-23 19:59:48 -0500

    SMTP

---
+++ src/02_TestMail.py
@@ -0,0 +1,37 @@
+"""
+Uso: SMTP
+Creador: Andrés Hernández Mata
+Version: 1.0.0
+Python: 3.9.1
+Fecha: 22 Junio 2021
+"""
+
+import smtplib
+import ssl
+import getpass
+
+smtp_server = "smtp.gmail.com"
+port = 587  # For starttls
+sender_email = "test.pc.fcfm@gmail.com"
+password = getpass.getpass()
+password = "fcfm.123"
+message = "Hello world!"
+
+# Create a secure SSL context
+context = ssl.create_default_context()
+
+# Try to log in to server and send email
+try:
+    server = smtplib.SMTP(smtp_server,port)
+    server.ehlo() # Can be omitted
+    server.starttls(context=context) # Secure the connection
+    server.ehlo() # Can be omitted
+    server.login(sender_email, password)
+    server.sendmail(sender_email, "receiver_email", message)
+except Exception as e:
+    # Print any error messages to stdout
+    print(e)
+finally:
+    server.quit()
+
+
